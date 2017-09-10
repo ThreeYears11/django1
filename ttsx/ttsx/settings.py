e554ecf10659ecb212e5bbcfceec0f5e11452b43
Sharon <xxyunx@163.com>
2017-09-10 14:53:28 +0800

    修改文件

--- ttsx/ttsx/settings.py
+++ ttsx/ttsx/settings.py
@@ -37,11 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
     'tt_user',
-=======
     'tt_cart',
->>>>>>> 202c12a85ffe2d951d9c2269f3aa63c5de539514
+    'tt_order',
 )
 
 MIDDLEWARE_CLASSES = (
