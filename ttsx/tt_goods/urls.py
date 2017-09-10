b0a8ce6db0467503e19c085a1d7c18febe0f0414
frank <wkmsxx@163.com>
2017-09-10 14:51:12 +0800

    frank

--- ttsx/tt_goods/urls.py
+++ ttsx/tt_goods/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
-from tt_goods import views
+from . import views
 
 urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^', views.index),
-
 ]
