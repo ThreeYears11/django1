6a91895080447183ddce88dfd28a1ac08626ea3e
XieT <1826117804@qq.com>
2017-09-11 09:31:30 +0800

    .....

--- ttsx/tt_cart/views.py
+++ ttsx/tt_cart/views.py
@@ -1,3 +1,10 @@
+from django.core.files.storage import FileSystemStorage
+from django.http import JsonResponse
 from django.shortcuts import render
+from tt_goods.models import *
+from tt_cart.models import *
 
 # Create your views here.
+
+
+
