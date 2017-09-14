af4790bd111f68dafe92bc66a18ea7fdc639ae40
XieT <1826117804@qq.com>
2017-09-14 11:02:19 +0800

    第一次上传

--- ttsx/tt_cart/urls.py
+++ ttsx/tt_cart/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-
+    url(r'^detail/$',views.detail),
+    url(r'^good_data/$',views.good_data),
+    url(r'^center/$',views.center),
+    url(r'^change_data1/$',views.change_data1),
+    url(r'^change_data2/$',views.change_data2),
+    url(r'^delete_data/$',views.delete_data),
 ]
--- ttsx/tt_goods/urls.py
+++ ttsx/tt_goods/urls.py
@@ -5,6 +5,6 @@ from tt_goods import views
 
 urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^', views.index),
+    url(r'^$', views.index),
 
 ]
