f6a0319424db6289234d079df0823c711cc3eea1
XieT <1826117804@qq.com>
2017-09-15 10:05:18 +0800

    完成商品和订单的对接

--- ttsx/tt_cart/urls.py
+++ ttsx/tt_cart/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-
-    url(r'^detail/$',views.detail),
     url(r'^good_data/$',views.good_data),
     url(r'^center/$',views.center),
     url(r'^change_data1/$',views.change_data1),
